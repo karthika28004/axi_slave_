40f2c32f503e855de9b911388026503c56a99efe
karthika28004 <karthika28004@gmail.com>
2026-06-01 19:08:12 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -1,18 +1,15 @@
 `default_nettype none
 `timescale 1ns / 1ps
-
 /* This testbench just instantiates the module and makes some convenient wires
    that can be driven / tested by the cocotb test.py.
 */
 module tb ();
-
   // Dump the signals to a FST file. You can view it with gtkwave or surfer.
   initial begin
     $dumpfile("tb.fst");
     $dumpvars(0, tb);
     #1;
   end
-
   // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
@@ -26,16 +23,13 @@ module tb ();
   wire VPWR = 1'b1;
   wire VGND = 1'b0;
 `endif
-
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-
+  tt_um_axi_slave_receiver user_project (
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
       .VPWR(VPWR),
       .VGND(VGND),
 `endif
-
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
@@ -45,5 +39,4 @@ module tb ();
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
-
 endmodule
